49f8865965865756d48be59d53c96fa7e9abb54a
karl-no <karl.publicidade@gmail.com>
2024-02-26 20:37:52 -0300

    chore: mudanças nos arquivos server.js e database.js

--- app/backend/src/config/database.js
+++ app/backend/src/config/database.js
@@ -12,11 +12,14 @@ const sequelize = new Sequelize(
 );
 
 const config = {
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
+  username: process.env.MYSQLUSER,
+  // username: process.env.DB_USER,
+  // password: process.env.DB_PASS || '123456',
+  password: process.env.MYSQL_ROOT_PASSWORD,
   database: process.env.DB_NAME,
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3002,
+  // host: process.env.DB_HOST || 'localhost',
+  host: process.env.MYSQLHOST,
+  port: Number(process.env.DB_PORT),
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
